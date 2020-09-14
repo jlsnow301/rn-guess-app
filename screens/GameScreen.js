503373e997a18b43901759ea64779cadfd9dce26
Jerm <42397676+jlsnow301@users.noreply.github.com>
2020-09-14 15:31:26 -0700

    New Screen

---
+++ screens/GameScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomInclusive = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomInclusive(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomInclusive(1, 100, props.userChoice)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
+
+export default GameScreen;
